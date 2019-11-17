a374f0219d00f80041b1d28306130f0143dc8646
Bijay Gaudel <gaudelbijay10@gmail.com>
2019-11-17 10:37:53 +0800

    nxgraph preprocessing function is added

--- utils.py
+++ utils.py
@@ -3,7 +3,14 @@ def partition_num(num,workers):
         return [num//workers]*workers
     else:
         return [num//workers]*workers+[num%workers]
-    
+
+def preprocess_nxgraph(graph):
+    node2idx = {}
+    idx2node = {}
+    for i,node in enumerate(graph.nodes()):
+        node2idx[node]=i
+        idx2node.append(node)
+    return idx2node,node2idx
 
 
         
